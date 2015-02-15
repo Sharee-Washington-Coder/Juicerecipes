6202177e2944ab6d188b43e9b8fed18cc32d22d9
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2015-02-15 18:01:30 -0500

    updated controllers & models

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -47,8 +47,8 @@ before_action :authenticate_user!, except: [:index, :show]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    #strong paramaters
+    # strong paramaters
     def recipe_params
-      params.require(:recipe).permit(:title, :veggie1, :veggie2, :fruit1, :fruit2, images_attributes: [:image])
+      params.require(:recipe).permit(:title, :veggie1, :veggie2, :fruit1, :fruit2, :image)
 end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-     validates_attachment_content_type :image, :content_type => /\Aimage\/(jpg|jpeg|pjpeg|png|x-png|gif)\z/
+     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
 end
