88665b300e02a943060b69c2b7c25712b69d84d8
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2015-02-15 17:00:57 -0500

    added strong parameters in models/recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+     validates_attachment_content_type :image, :content_type => /\Aimage\/(jpg|jpeg|pjpeg|png|x-png|gif)\z/
       validates :veggie1, :veggie2, :fruit1, :fruit2, :image, presence: true 
 end
