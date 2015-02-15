3511440e06763b05f7fa4e40e0186d8c9ad3d384
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2015-02-15 18:33:52 -0500

    updated apps/config/public/assets

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
+     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+     validates :image, presence: true
 end
