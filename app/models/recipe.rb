ceacf52b3f680f725cacbf57212c51e4ca196d49
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2015-02-16 18:16:03 -0500

    modified controller/models/views/config/migrate

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,5 @@ class Recipe < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
      validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-     validates :image, :veggie1, :veggie2, :fruit1, :fruit2, presence: true
+     validates :veggie1, :veggie2, :fruit1, :fruit2, :image, presence: true
 end
